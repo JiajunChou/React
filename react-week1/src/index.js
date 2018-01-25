c9fe059c21cfa9feaae68869b682dfa986e702de
jiajun.chpu <jiajun.chou@104.com.tw>
2018-01-25 14:47:27 +0800

    Extract Component: Divid UserInfo from Comment

--- react-week1/src/index.js
+++ react-week1/src/index.js
@@ -95,46 +95,46 @@ function formatDate(date) {
     return date.toLocaleDateString();
   }
   
-  function Comment(props) {
-    return (
-      <div className="Comment">
-        <div className="UserInfo">
-          <img
-            className="Avatar"
-            src={props.author.avatarUrl}
-            alt={props.author.name}
-          />
-          <div className="UserInfo-name">
-            {props.author.name}
-          </div>
-        </div>
-        <div className="Comment-text">
-          {props.text}
-        </div>
-        <div className="Comment-date">
-          {formatDate(props.date)}
-        </div>
+function UserInfo(props) {
+    return(
+        <div className="UserInfo">        
+          <img className="Avatar" src={props.author.avatarUrl} alt={props.author.name} />
+          <div className="UserInfo-name">{props.author.name}</div>
+        </div>        
+    )
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      {UserInfo(props)}
+      <div className="Comment-text">
+        {props.text}
       </div>
-    );
-  }
-  
-  const comment = {
-    date: new Date(),
-    text:
-      'I hope you enjoy learning React!',
-    author: {
-      name: 'Hello Kitty',
-      avatarUrl:
-        'http://placekitten.com/g/64/64',
-    },
-  };
-  ReactDOM.render(
-    <Comment
-      date={comment.date}
-      text={comment.text}
-      author={comment.author}
-    />,
-    document.getElementById('Extract_Compon')
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
   );
+}
+
+const comment = {
+  date: new Date(),
+  text:
+    'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl:
+      'http://placekitten.com/g/64/64',
+  },
+};
+ReactDOM.render(
+  <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author}
+  />,
+  document.getElementById('Extract_Compon')
+);
   
 registerServiceWorker();
