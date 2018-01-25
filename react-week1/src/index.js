877bebe1bef53e6d74143701fefb120473f35994
jiajun.chpu <jiajun.chou@104.com.tw>
2018-01-25 11:40:43 +0800

    JSX Intro: Greeting

--- react-week1/src/index.js
+++ react-week1/src/index.js
@@ -5,9 +5,33 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
+/**Hello World */
 ReactDOM.render(
-    <h1>Hello, World</h1>,
+    <h1>Hello, 
+    Big PIRATE ERA</h1>,
     document.getElementById('Hello_World')
 );
+
+/** Greeting */
+
+function formatName(someone){
+    return someone.firstName + '.D.' + someone.lastName;
+}
+
+const person1 = {
+    firstName: 'Monkey',
+    lastName: 'Luffe'
+}
+
+const Greeting = (
+    <h2>
+        Good Morning, {formatName(person1)}
+    </h2>
+)
+
+ReactDOM.render(
+    Greeting,
+    document.getElementById('Greeting')
+)
+
 registerServiceWorker();
