63bd636bc3a6c0d4fb5e9f045f3298c25486cf18
jiajun.chpu <jiajun.chou@104.com.tw>
2018-02-01 14:07:48 +0800

    Lifting State Up: Adding a Second Input

--- react-week1/src/LiftingStateUp.js
+++ react-week1/src/LiftingStateUp.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 var BoilingVerdict = function(props){
-    if(props.WaterTemperacture >= 100){
+    if( (props.type === "Celsius" && props.WaterTemperacture >= 100) || (props.type === "Fahrenheit" && props.WaterTemperacture >= 212)){
         return <p>The water would boil.</p>;
     }
     return <p>The water would not boil.</p>
@@ -11,19 +11,25 @@ class LiftingStateUp extends Component{
     constructor(props){
         super(props);
         this.state = {
-            temperacture: ""
+            Celsius: "",
+            Fahrenheit: ""
         };
     }
 
     handleChange = (e) => {
-        this.setState({temperacture: e.target.value});
+        const name=e.target.name;
+        this.setState({[name]: e.target.value});
     }
 
     render(){
         return(
             <div>
-                <input name="Temperacture" type="text" value={this.state.temperacture} onChange={this.handleChange} />
-                <BoilingVerdict WaterTemperacture = {this.state.temperacture} />
+                <legend>Enter temperature in Celsius: </legend>
+                <input name="Celsius" type="text" value={this.state.Celsius} onChange={this.handleChange} />
+                <BoilingVerdict type="Celsius" WaterTemperacture = {this.state.Celsius} />
+                <legend>Enter temperature in Fahrenheit: </legend>
+                <input name="Fahrenheit" type="text" value={this.state.Fahrenheit} onChange={this.handleChange} />
+                <BoilingVerdict type="Fahrenheit" WaterTemperacture = {this.state.Fahrenheit} />
             </div>
         )
     }
