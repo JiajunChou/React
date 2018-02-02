c9d4a6feebcf8e5682cbae26df1e5839692f67d2
jiajun.chpu <jiajun.chou@104.com.tw>
2018-02-02 14:30:42 +0800

    HomeWork: LoggingForm with css

--- react-week1/src/index.js
+++ react-week1/src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Calculator  from './LiftingStateUp';
+import './LogginForm.css';
+import LoggingForm from './LogginForm';
+// import Calculator  from './LiftingStateUp';
 import registerServiceWorker from './registerServiceWorker';
 
 registerServiceWorker();
 // ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Calculator  />, document.getElementById('LiftingStateUp'));
+ReactDOM.render(<LoggingForm />, document.getElementById('Loggin') );
+// ReactDOM.render(<Calculator  />, document.getElementById('LiftingStateUp'));
