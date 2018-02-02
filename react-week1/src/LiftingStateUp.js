32a3bb99a1f42cb0bc4953f43a46b586fe4edb4d
jiajun.chpu <jiajun.chou@104.com.tw>
2018-02-02 13:57:53 +0800

    Lifting State Up: Add two convert functions

--- react-week1/src/LiftingStateUp.js
+++ react-week1/src/LiftingStateUp.js
@@ -7,35 +7,47 @@ var BoilingVerdict = function(props){
     return <p>The water would not boil.</p>
 }
 
+var FahrenheitToCelsius = function(props){
+    return (props.Temperature - 32) * 5/9;
+}
+
+var CelsiusToFahrenheit = function(props){
+    return (props.Tempera * 9/5) + 32;
+}
+
+var Convertor = function(temperature, Convert){
+    if(Number.isNaN(temperature)) return "";
+    const result = Convert(parseFloat(temperature));
+    return Math.round(result).toString();
+}
+
 class TemperatureInput extends Component{
     constructor(props){
         super(props);
         this.state = {
-            Temperateure: "",
+            Temperature: "",
         };
     }
 
     handleChange = (e) => {
-        this.setState({Temperateure: e.target.value});
+        this.setState({Temperature: e.target.value});
     }
 
     render(){
         return(
             <div>
                 <legend>Enter temperature in {this.props.TempType}: </legend>
-                <input name={this.props.TempType} type="text" value={this.state.Temperateure} onChange={this.handleChange} />
-                <BoilingVerdict type={this.props.TempType} WaterTemperacture = {this.state.Temperateure} />
+                <input name={this.props.TempType} type="text" value={this.state.Temperature} onChange={this.handleChange} />
+                <BoilingVerdict type={this.props.TempType} WaterTemperacture = {this.state.Temperature} />
             </div>
         )
     }
 }
 
-
 const scaleName = {
     c: "Celsius",
     f: "Fahrenheit"
 };
-
 
 class Calculator extends Component{
 
