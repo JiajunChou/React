390d1fe05d9cfb3f039f7af83764b1685595790e
jiajun.chpu <jiajun.chou@104.com.tw>
2018-02-25 12:06:09 +0800

    Reorganization material

--- react-week1/src/index.js
+++ react-week1/src/index.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import './LogginForm.css';
-// import LoggingForm from './LogginForm';
-// import Calculator  from './LiftingStateUp';
-import PageView from './ListInfo';
-import registerServiceWorker from './registerServiceWorker';
+import './App/index.css';
+import App from './App/App';
 
+import registerServiceWorker from './App/registerServiceWorker';
 registerServiceWorker();
-// ReactDOM.render(<App />, document.getElementById('root'));
-// ReactDOM.render(<LoggingForm />, document.getElementById('Loggin') );
-// ReactDOM.render(<Calculator  />, document.getElementById('LiftingStateUp'));
 
-// import ListInfo from './component/UserInfo';
-
-ReactDOM.render(
-    <PageView />,
-    document.getElementById('Array')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
