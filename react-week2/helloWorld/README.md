c8d35599161f84107b559df1f13b16535b283319
jiajun.chpu <jiajun.chou@104.com.tw>
2018-03-13 17:27:20 +0800

    Hello World

---
+++ react-week2/helloWorld/README.md
@@ -0,0 +1,6 @@
+# Hello World
+
+1. HelloWorld程式碼說明
+1. 基本的開發與執行的流程
+1. JSX語法與React Element(元素)
+1. React Component(元件)語法
---
+++ react-week2/helloWorld/src/App/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import HelloWorld from './HelloWorld';
+
+class App extends Component {
+  render() {
+    return (
+      <HelloWorld text="Hello World, 今天重新學 React" />
+    );
+  }
+}
+
+export default App;
